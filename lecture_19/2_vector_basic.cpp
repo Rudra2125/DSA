1074731de26c770b60e2c1e6598a7a7032e3bf54
Rudra2125 <rudrasingh2125@gmail.com>
2026-02-10 15:55:01 +0530

    g

--- lecture_19/2_vector_basic.cpp
+++ lecture_19/2_vector_basic.cpp
@@ -11,10 +11,18 @@ int main()
    a.push_back(3); 
    a.push_back(4); 
    a.push_back(5); 
+   //size
     cout<<a.size()<<endl;
+    //capacity
     cout<<a.capacity()<<endl;
+    //delete
+    a.pop_back();
     for(int i=0; i<a.size(); i++){
         cout<<a[i]<<" ";
     }
+    //first element
+    cout<<a.front()<<endl;
+    //last element
+    cout<<a.back()<<endl;
     return 0;
 }
